c786f58ed11eec0a3340401d7a82ecffcb0f4717
vincentcarbonaro <nd4spd356@gmail.com>
2015-06-13 23:29:19 -0400

    Progress 6/13, need to fix footer

---
+++ README.md
@@ -0,0 +1,7 @@
+git status ## tell you what files have been changed
+
+git add -A ## will que those files up to be commited
+
+git commit -m "WHATEVER MESSAGE" ## commit the files to be pushed
+
+git push origin ## pushes to github, will prompt for password
--- js/site.js
+++ js/site.js
@@ -1,16 +1,13 @@
-$(document).ready(function() {
 $(function () {
 
   $('nav ul li').click(function () {
 
     $('.content').children().each(function () {
-      $(this).toggle(false)
+      $(this).toggle(false);
     });
 
-    $('#' + this.classList[0]).toggle(true)
-
+    $('#' + this.classList[0]).toggle(true);
+    console.log(this.classList[0]);
   });
-
-});
 
 });
