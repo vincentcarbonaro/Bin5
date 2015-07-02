c0a33cfcdbce03a998ad6419e074252c747e03f3
vincentcarbonaro <nd4spd356@gmail.com>
2015-07-02 19:58:32 -0400

    Got parallax working + titles + added parallax-mirror to js

--- js/site.js
+++ js/site.js
@@ -1,12 +1,16 @@
 $(function () {
 
   $('nav ul li').click(function () {
-
     $('.content').children().each(function () {
       $(this).toggle(false);
     });
 
     $('#' + this.classList[0]).toggle(true);
+    if(this.classList[0] == "about") {
+      $('.parallax-mirror').toggle(true);
+    } else {
+      $('.parallax-mirror').toggle(false);
+    }
   });
 
 });
